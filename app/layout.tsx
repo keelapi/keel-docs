357432449567e0a6c7424d6d9ec7f52653c8a9a3
sftimeless <37782990+sftimeless@users.noreply.github.com>
2026-03-26 16:50:49 -0700

    Standardize docs page titles to 'Keel Docs | {Page Title}' via global metadata template

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: {
     default: 'Keel Docs',
-    template: '%s | Keel Docs'
+    template: 'Keel Docs | %s'
   },
   icons: {
     icon: [
